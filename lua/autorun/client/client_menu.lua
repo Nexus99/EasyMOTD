0dfc00bc34c9b856e3a6ef15e3a7c6e31bf4b0e9
JrLemonsHD <jrlemonshd@gmail.com>
2014-08-03 05:33:42 -0500

    Fixed and optimized for more screen sizes

--- lua/autorun/client/client_menu.lua
+++ lua/autorun/client/client_menu.lua
@@ -2,37 +2,26 @@ local colors = {
 	head = Color(192, 57, 43, 255),
 	back = Color(236, 240, 241, 255),
 	text = Color(255, 255, 255, 255),
-	text_blue = Color(52, 152, 219, 255),
 	btn = Color(52, 73, 94, 255),
-	btn_hover = Color(44, 62, 80, 255),
-	btn_disabled = Color(52, 73, 94, 150),
-	open = Color(46, 204, 113, 255),
-	open_hover = Color(39, 174, 96, 255),
-	open_disabled = Color(46, 2014, 113, 150),
-	cancel = Color(231, 76, 60, 255),
-	cancel_hover = Color(192, 57, 43, 255),
-	bar = Color(189, 195, 199, 255),
-	barup = Color(127, 140, 141, 255),
-	closed = Color(230, 126, 34, 255),
-	closed_hover = Color(211, 84, 0, 255),
-	info_back = Color(189, 195, 199, 255),
-	gray = Color(189, 195, 199, 255),
-	gray_hover = Color(127, 140, 141, 255),
 }
 
 
 
 
 surface.CreateFont("menuHead", {font = "OpenSans-Bold", size = 60, weight = 500})
-surface.CreateFont("menukBtn", {font = "OpenSans-Regular", size = 30, weight = 500})
+surface.CreateFont("menuBtn", {font = "OpenSans-Regular", size = 30, weight = 500})
 surface.CreateFont("menuBtnSmall", {font = "OpenSans-Light", size = 15, weight = 500})
 
 
 function menu()
 
+
+
+
 dframe = vgui.Create("DFrame")
 dframe:SetSize(ScrW(), ScrH())
 dframe:SetTitle("")
+dframe:MoveToFront()
 dframe:Center()
 dframe:SetDraggable( false )
 dframe:MakePopup()
@@ -41,16 +30,19 @@ dframe:ShowCloseButton(false)
 dframe.Paint = function()
 	draw.RoundedBox(0, 0, 0, dframe:GetWide(), dframe:GetTall(), colors.back)
 	draw.RoundedBox(0, 0, 0, dframe:GetWide(), 100, colors.head)
-	draw.SimpleText("Welcome to SERVER NAME HERE!", "menuHead", dframe:GetWide() / 2, 20, colors.text, TEXT_ALIGN_CENTER)
+	draw.SimpleText("Welcome to the MOTD Test Server!", "menuHead", dframe:GetWide() / 2, 20, colors.text, TEXT_ALIGN_CENTER)
+	dframe:MoveToFront()
 end
 
 local HTML = vgui.Create("HTML", dframe)
-HTML:SetSize( ScrW(), ScrH() - 150  )
-HTML:SetPos( ScrW() - 1922 , ScrH() / 10.7)
-HTML:OpenURL("www.google.com") --- Your Website Here!
+HTML:SetSize( ScrW(), ScrH() )
+HTML:Center()
+HTML:SetPos( 0, 100)
+HTML:OpenURL("http://facepunch.com/showthread.php?t=1414515") --- Your Website Here!
 
 local dbutton = vgui.Create("DButton", dframe)
 dbutton:SetText("")
+dbutton:MoveToFront()
 dbutton:SetSize(250, 50)
 dbutton:SetPos(dframe:GetWide() - 250, dframe:GetTall() - 50)
 dbutton.DoClick = function()
@@ -61,9 +53,9 @@ end
 
 dbutton.Paint = function()
 	draw.RoundedBox(0, 0, 0, dbutton:GetWide(), dbutton:GetTall(), colors.head);
-	draw.SimpleText("Close", "stockBtn", dbutton:GetWide() / 2, 10, colors.text, TEXT_ALIGN_CENTER);
+	draw.SimpleText("Close", "menuBtn", dbutton:GetWide() / 2, 10, colors.text, TEXT_ALIGN_CENTER);
+	dbutton:MoveToFront()
 end
-
 
 end
 
@@ -76,3 +68,6 @@ end
 
 concommand.Add("open_menu", menu)
 concommand.Add("close_menu", closemenu)
+
+
+
