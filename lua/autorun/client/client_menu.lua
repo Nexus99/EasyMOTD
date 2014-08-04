88ffdbb5b393ffac7abe4603b6aa6dc6dd58477b
JrLemonsHD <jrlemonshd@gmail.com>
2014-08-03 19:36:36 -0500

    To satisfy Crazy and Penguin

--- lua/autorun/client/client_menu.lua
+++ lua/autorun/client/client_menu.lua
@@ -1,70 +1,70 @@
 local colors = {
-	head = Color(192, 57, 43, 255),
-	back = Color(236, 240, 241, 255),
-	text = Color(255, 255, 255, 255),
-	btn = Color(52, 73, 94, 255),
+        head = Color(192, 57, 43, 255),
+        back = Color(236, 240, 241, 255),
+        text = Color(255, 255, 255, 255),
+        btn = Color(52, 73, 94, 255),
 }
-
-
-
-
+ 
+ 
+ 
+ 
 surface.CreateFont("menuHead", {font = "OpenSans-Bold", size = 60, weight = 500})
 surface.CreateFont("menuBtn", {font = "OpenSans-Regular", size = 30, weight = 500})
 surface.CreateFont("menuBtnSmall", {font = "OpenSans-Light", size = 15, weight = 500})
-
-
-function menu()
-
-
-
-
-dframe = vgui.Create("DFrame")
-dframe:SetSize(ScrW(), ScrH())
-dframe:SetTitle("")
-dframe:MoveToFront()
-dframe:Center()
-dframe:SetDraggable( false )
-dframe:MakePopup()
-dframe:SetVisible( true )
-dframe:ShowCloseButton(false)
-dframe.Paint = function()
-	draw.RoundedBox(0, 0, 0, dframe:GetWide(), dframe:GetTall(), colors.back)
-	draw.RoundedBox(0, 0, 0, dframe:GetWide(), 100, colors.head)
-	draw.SimpleText("Welcome to SERVER NAME HERE!", "menuHead", dframe:GetWide() / 2, 20, colors.text, TEXT_ALIGN_CENTER)
-	dframe:MoveToFront()
+ 
+local panel = nil;
+ 
+local function menu()
+ 
+        local dframe = vgui.Create("DFrame")
+        dframe:SetSize(ScrW(), ScrH())
+        dframe:SetTitle("")
+        dframe:MoveToFront()
+        dframe:Center()
+        dframe:SetDraggable( false )
+        dframe:MakePopup()
+        dframe:SetVisible( true )
+        dframe:ShowCloseButton(false)
+        dframe.Paint = function()
+                draw.RoundedBox(0, 0, 0, dframe:GetWide(), dframe:GetTall(), colors.back)
+                draw.RoundedBox(0, 0, 0, dframe:GetWide(), 100, colors.head)
+                draw.SimpleText("Welcome to SERVER NAME HERE!", "menuHead", dframe:GetWide() / 2, 20, colors.text, TEXT_ALIGN_CENTER)
+                dframe:MoveToFront()
+        end
+ 
+        panel = dframe;
+ 
+        local HTML = vgui.Create("HTML", dframe)
+        HTML:SetSize( ScrW(), ScrH() )
+        HTML:Center()
+        HTML:SetPos( 0, 100)
+        HTML:OpenURL("http://facepunch.com/showthread.php?t=1414515") --- Your Website Here!
+ 
+        local dbutton = vgui.Create("DButton", dframe)
+        dbutton:SetText("")
+        dbutton:MoveToFront()
+        dbutton:SetSize(250, 50)
+        dbutton:SetPos(dframe:GetWide() - 250, dframe:GetTall() - 50)
+        dbutton.DoClick = function()
+                panel:Close();
+        end
+ 
+        dbutton.Paint = function()
+                draw.RoundedBox(0, 0, 0, dbutton:GetWide(), dbutton:GetTall(), colors.head);
+                draw.SimpleText("Close", "menuBtn", dbutton:GetWide() / 2, 10, colors.text, TEXT_ALIGN_CENTER);
+                dbutton:MoveToFront()
+        end
+ 
 end
-
-local HTML = vgui.Create("HTML", dframe)
-HTML:SetSize( ScrW(), ScrH() )
-HTML:Center()
-HTML:SetPos( 0, 100)
-HTML:OpenURL("http://facepunch.com/showthread.php?t=1414515") --- Your Website Here!
-
-local dbutton = vgui.Create("DButton", dframe)
-dbutton:SetText("")
-dbutton:MoveToFront()
-dbutton:SetSize(250, 50)
-dbutton:SetPos(dframe:GetWide() - 250, dframe:GetTall() - 50)
-dbutton.DoClick = function()
-	dframe:SetVisible( false )
-	dframe:MakePopup( false )
-	RunConsoleCommand("close_menu")
+ 
+local function closemenu()
+        if (!panel) then return; end
+ 
+        panel:Remove()
+        panel = nil;
 end
-
-dbutton.Paint = function()
-	draw.RoundedBox(0, 0, 0, dbutton:GetWide(), dbutton:GetTall(), colors.head);
-	draw.SimpleText("Close", "menuBtn", dbutton:GetWide() / 2, 10, colors.text, TEXT_ALIGN_CENTER);
-	dbutton:MoveToFront()
-end
-
-end
-
-function closemenu()
-	dframe:SetVisible( false )
-	dframe:MakePopup( false )
-end
-
-
-
+ 
+ 
+ 
 concommand.Add("open_menu", menu)
 concommand.Add("close_menu", closemenu)
